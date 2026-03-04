e2327d4c9809180336dfa934b45d96a1c425c292
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-04 21:18:34 +0000

    basic path param req dn running | err 422 if invalid data

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ app = FastAPI()
 def read_root():
     return {"message": "Welcome to FastAPI!"}
 
-
 #hello get request
 @app.get("/hello")
 def say_hello():
     return {"message": "Hello, FastAPI!"}
 
+#path parameter get request
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id}
 
