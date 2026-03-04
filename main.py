9d1873eaa79086aa274244f54833e6a0a26c0f89
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-03-04 21:22:39 +0000

    multiple path parameters get request

--- main.py
+++ main.py
@@ -17,3 +17,8 @@ def say_hello():
 def read_item(item_id: int):
     return {"item_id": item_id}
 
+#multiple path parameters get request
+@app.get("/users/{user_id}/posts/{post_id}")
+def read_user_post(user_id: int, post_id: int):
+    return {"user_id": user_id, "post_id": post_id}
+
